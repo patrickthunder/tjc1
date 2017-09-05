52f9d0833033069b808e83dda87ee342dd550f3e
patrick thunder <patrickthunder@gmail.com>
2017-09-05 12:54:58 -0700

    my commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
+
+//alert("hello TJC");
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
