749ea64d793d00186bd2790a845f83f1450f31cb
patrick thunder <patrickthunder@gmail.com>
2017-10-24 16:59:03 -0700

    sweet alert2 install success with web pack
    sweet alert 2 install success with web pack

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,11 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
+//sweet alert2
+import swal from 'sweetalert2/dist/sweetalert2.all.min.js'
+//sweet alert function swal
+swal('Hi from webpack!')
+
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
