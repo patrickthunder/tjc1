c156583124aa4f98717c999a777e4b5c7d8efcac
patrick thunder <patrickthunder@gmail.com>
2017-10-24 17:57:02 -0700

    import sweetalert2 and add event listeners
    import sweet alert2 via web pack and added event listeners with alerts
    for form error an success

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,10 +3,9 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
-//sweet alert2
+//sweet alert2 import node module
 import swal from 'sweetalert2/dist/sweetalert2.all.min.js'
-//sweet alert function swal
-swal('Hi from webpack!')
+
 
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
@@ -15,3 +14,31 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
+
+//contact form id event listener with abide
+$('#contact-form')
+
+  // field element is invalid function
+  .on("invalid.zf.abide", function(ev,elem) {
+    //add sweet alert function swal for error
+    swal(
+      'Oops...',
+      'Something went wrong!',
+      'error'
+    )
+  })
+  // form validation passed, form will submit if submit event not returned false
+  .on("formvalid.zf.abide", function(ev,frm) {
+    //add sweet alert function swal for successful validation
+    swal(
+      'Good Job',
+      'You sent the form!',
+      'success'
+    )
+    // ajax post form
+  })
+  // to prevent form from submitting upon successful validation
+  .on("submit", function(ev) {
+    ev.preventDefault();
+    console.log("Submit for form id "+ev.target.id+" intercepted");
+  });
